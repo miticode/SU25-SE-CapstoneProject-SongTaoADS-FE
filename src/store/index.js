2d301a430d2c74152ebadd9dd47a9d8be65c5f1c
trideptrai <lehuynhminhtri09082003@gmail.com>
2025-06-03 00:49:41 +0700

    Admin Dashboard

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/auth/authSlice';
-import productTypeReducer from './features/productType/productTypeSlice';
-import customerReducer from './features/customer/customerSlice';
-import attributeReducer from './features/attribute/attributeSlice';
-import orderReducer from './features/order/orderSlice';
-import designTemplateReducer from './features/designTemplate/designTemplateSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/auth/authSlice";
+import productTypeReducer from "./features/productType/productTypeSlice";
+import customerReducer from "./features/customer/customerSlice";
+import attributeReducer from "./features/attribute/attributeSlice";
+import orderReducer from "./features/order/orderSlice";
+import designTemplateReducer from "./features/designTemplate/designTemplateSlice";
+import userReducer from "./features/user/userSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -12,10 +13,10 @@ export const store = configureStore({
     customers: customerReducer,
     attribute: attributeReducer,
     order: orderReducer,
-      designTemplate: designTemplateReducer,
+    designTemplate: designTemplateReducer,
+    users: userReducer,
     // Thêm các reducer khác tại đây
-  }
+  },
 });
 
 export default store;
-
